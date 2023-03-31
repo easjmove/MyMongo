39b17caa4fe450cc30e3b618361313f5e9ada52e
move <move@zealand.dk>
2023-03-31 10:12:39 +0200

    Add project files.

---
+++ MyMongo/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+Console.WriteLine("Mongo DB Test");
+
+var client = new MongoClient();
+
+foreach (var database in client.ListDatabases().ToList())
+{
+    //  Console.WriteLine(database);
+}
+
+Console.WriteLine("\r\n");
+
+var imdb = client.GetDatabase("IMDB");
+
+foreach (var collection in imdb.ListCollections().ToList())
+{
+    //Console.WriteLine(collection);
+}
+
+var principlesCollection = imdb.GetCollection<BsonDocument>("titleprincipals");
+
+var principles = principlesCollection.Find("{category:\"self\"}").Limit(10).ToList();
+
+foreach (var principle in principles)
+{
+    //Console.WriteLine(principle);
+}
+
+var titles = imdb.GetCollection<BsonDocument>("TitlesBasic");
+
+var searchResult = titles.Aggregate().Lookup("titleprincipals",
+    "tconst", "tconst", "principles").Limit(10).ToList();
+
+foreach (var title in searchResult)
+{
+    Console.WriteLine(title);
+}
